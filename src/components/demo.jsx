14c89452ab2902a3c80e0f1898c8344037c702f6
Prafull-Garg-11 <prafullgarg.it19@aryacollege.in>
2022-05-05 14:19:39 +0530

    Start

---
+++ src/components/demo.jsx
@@ -0,0 +1,12 @@
+import style  from "./demo.module.css"
+import Test from "./test.jsx"
+const Demo=()=>{
+    return(
+        <>
+        <h2 className={style.s}>This is the first function demo</h2>
+        <Test></Test>
+        </>
+    );
+}
+
+export default Demo;
---
+++ src/components/test.jsx
@@ -0,0 +1,10 @@
+import style from "./test.module.css";
+const Test=()=>{
+    return (
+        <>
+        <h3 className={style.a}>second function </h3>
+        </>
+    );
+}
+
+export default Test;
